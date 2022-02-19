141640c41f2f7b5542140b1248678a43e2a85f31
caio.cp <caio.cesar@itarget.com.br>
2022-02-19 11:38:47 -0300

    feat/Função getItem inserida na lista, mas precisa ser melhorada para criar novos itens

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,12 +4,24 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 const Item = () => {
+  const [storageIdentifier, setStorageIdentifier] = React.useState("");
+  const [storagePreco, setStoragePreco] = React.useState("");
+  const [storageLocal, setStorageLocal] = React.useState("");
+
+  React.useEffect(() => {
+    setStorageIdentifier(localStorage.getItem("identifier"));
+
+    setStoragePreco(localStorage.getItem("preco"));
+
+    setStorageLocal(localStorage.getItem("local"));
+  }, [storageIdentifier, storagePreco, storageLocal]);
+
   return (
     <tbody>
       <tr className="entrega">
-        <td className="id">model</td>
-        <td>model</td>
-        <td>model</td>
+        <td>{storageIdentifier}</td>
+        <td>R${storagePreco}</td>
+        <td>{storageLocal}</td>
         <td>
           <Button size="small" color="secondary" startIcon={<EditIcon />}>
             edit
