d2e783fda5742b255abcd3e469f5febb71bba6d2
caio.cp <caio.cesar@itarget.com.br>
2022-02-21 12:00:45 -0300

    fix/reestruturando coisas e planejando interações

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,32 +5,52 @@ import DeleteIcon from "@mui/icons-material/Delete";
 
 const Item = () => {
   const [storageIdentifier, setStorageIdentifier] = React.useState("");
-  const [storagePreco, setStoragePreco] = React.useState("");
+  const [storageCliente, setStorageCliente] = React.useState("");
   const [storageLocal, setStorageLocal] = React.useState("");
 
   React.useEffect(() => {
     setStorageIdentifier(localStorage.getItem("identifier"));
 
-    setStoragePreco(localStorage.getItem("preco"));
+    setStorageCliente(localStorage.getItem("cliente"));
 
     setStorageLocal(localStorage.getItem("local"));
-  }, [storageIdentifier, storagePreco, storageLocal]);
+  }, [storageIdentifier, storageCliente, storageLocal]);
+
+  const lista = [];
+
+  const handleEdit = () => {};
+
+  const handleDelete = () => {};
 
   return (
     <tbody>
-      <tr className="entrega">
-        <td>{storageIdentifier}</td>
-        <td>R${storagePreco}</td>
-        <td>{storageLocal}</td>
-        <td>
-          <Button size="small" color="secondary" startIcon={<EditIcon />}>
-            edit
-          </Button>
-          <Button size="small" color="secondary" startIcon={<DeleteIcon />}>
-            del
-          </Button>
-        </td>
-      </tr>
+      {lista.map((item) => {
+        return (
+          <tr key={item.id} className="entrega">
+            <td>{item.identifier}</td>
+            <td>{item.cliente}</td>
+            <td>{item.local}</td>
+            <td>
+              <Button
+                size="small"
+                color="secondary"
+                startIcon={<EditIcon />}
+                onClick={handleEdit}
+              >
+                edit
+              </Button>
+              <Button
+                size="small"
+                color="secondary"
+                startIcon={<DeleteIcon />}
+                onClick={handleDelete}
+              >
+                del
+              </Button>
+            </td>
+          </tr>
+        );
+      })}
     </tbody>
   );
 };
